c881fa4242ea311e55e8808642e3248e56a71bff
tofayeLL <ahmedtofayel132@gmail.com>
2024-08-11 13:20:05 +0600

    install initials packages

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-5xl font-semibold bg-red-500'>My Portfolio</h1>
+      <button className='btn btn-secondary'>Daisy ui</button>
+    
+     
+    </>
+  )
+}
+
+export default App
