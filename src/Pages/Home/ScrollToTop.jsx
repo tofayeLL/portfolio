502a132ea8c14453d22b89d6e811c520496b5466
tofayeLL <ahmedtofayel132@gmail.com>
2024-11-14 13:40:59 +0600

    finally finished all the section updated

--- src/Pages/Home/ScrollToTop.jsx
+++ src/Pages/Home/ScrollToTop.jsx
@@ -39,9 +39,9 @@ const ScrollToTop = () => {
                     isVisible && (
                         <button
                             onClick={scrollToTop}
-                            className="fixed bottom-10  lg:right-4 right-0 bg-purple-800 text-white p-3 rounded-full shadow-md hover:bg-purple-900 transition"
+                            className="fixed bottom-10  lg:right-4 right-0 bg-purple-700 text-white p-3 rounded-full shadow-md hover:bg-purple-900 transition"
                         >
-                            <FaArrowUp lg:size={22} size={17}/>
+                            <FaArrowUp size={22} />
                         </button>
                     )}
 
--- src/Pages/Nav/Nav.jsx
+++ src/Pages/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-scroll";
-import logo from '../../assets/images/logos/update8.png'
+
 
 
 
@@ -10,7 +10,7 @@ const Nav = () => {
 
 
 
-            <div className="navbar fixed z-10    lg:text-slate-200 text-black    py-2 lg:px-14">
+<div className="navbar fixed z-10 text-white   bg-gradient-to-r from-[rgba(25,0,42,0.8)] to-[rgba(28,5,37,0.8)] py-2 lg:px-14">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
@@ -29,113 +29,113 @@ const Nav = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content  rounded-box z-[1] mt-3 w-40 p-2 shadow  space-y-3 font-semibold bg-white">
+                            className="menu menu-sm dropdown-content   z-[1] w-[100vh] p-2 shadow  space-y-3 font-semibold bg-gradient-to-r from-[rgba(25,0,42,0.8)] to-[rgba(28,5,37,0.8)]">
 
-                              <Link
-                            to="home"
-                            smooth={true}
-                            duration={1000}
-                           
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Home
-                        </Link>
-                      
-                        <Link
-                            to="about"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            About
-                        </Link>
+                            <Link
+                                to="home"
+                                smooth={true}
+                                duration={1000}
 
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Home
+                            </Link>
 
-                        <Link
-                            to="skills"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Skills
-                        </Link>
+                            <Link
+                                to="about"
+                                smooth={true}
+                                duration={1000}
+
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                About
+                            </Link>
 
 
-                        <Link
-                            to="projects"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Projects
-                        </Link>
+                            <Link
+                                to="skills"
+                                smooth={true}
+                                duration={1000}
+
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Skills
+                            </Link>
+
+
+                            <Link
+                                to="projects"
+                                smooth={true}
+                                duration={1000}
+
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Projects
+                            </Link>
 
 
 
-                         <Link
-                            to="education"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Education
-                        </Link>
+                            <Link
+                                to="education"
+                                smooth={true}
+                                duration={1000}
 
-                        <Link
-                            to="blogs"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base hidden font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Blogs
-                        </Link>
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Education
+                            </Link>
+
+                            <Link
+                                to="blogs"
+                                smooth={true}
+                                duration={1000}
+
+                                className="text-base hidden font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Blogs
+                            </Link>
 
 
-                        <Link
-                            to="contact"
-                            smooth={true}
-                            duration={1000}
-                            
-                            className="text-base font-semibold hover:text-purple-600 cursor-pointer"
-                        >
-                            Contact
-                        </Link>
+                            <Link
+                                to="contact"
+                                smooth={true}
+                                duration={1000}
 
-                            
+                                className="text-base font-semibold hover:text-purple-600 cursor-pointer"
+                            >
+                                Contact
+                            </Link>
+
+
 
 
 
                         </ul>
                     </div>
                     <a className="text-4xl lg:block hidden font-bold  font-mono"><span className="text-purple-600 ">Tofayel</span> Ahmed</a>
-                    
+
                 </div>
                 <a className="btn btn-ghost text-xl text-white  lg:hidden navbar-end "><span className="text-purple-600 ">Tofayel</span> Ahmed</a>
-                
+
                 <div className="lg:navbar-end hidden lg:flex ">
                     <ul className="menu menu-horizontal px-1 space-x-6  text-white font-mono lg:mr-3">
 
-                      {/* this "Link" import from react scroll not react dom */}
-                       <Link
+                        {/* this "Link" import from react scroll not react dom */}
+                        <Link
                             to="home"
                             smooth={true}
                             duration={1000}
-                           
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Home
                         </Link>
-                      
+
                         <Link
                             to="about"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             About
@@ -145,7 +145,7 @@ const Nav = () => {
                             to="skills"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Skills
@@ -156,7 +156,7 @@ const Nav = () => {
                             to="projects"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Projects
@@ -164,11 +164,11 @@ const Nav = () => {
 
 
 
-                         <Link
+                        <Link
                             to="education"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Education
@@ -178,7 +178,7 @@ const Nav = () => {
                             to="blogs"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium  hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Blogs
@@ -189,7 +189,7 @@ const Nav = () => {
                             to="contact"
                             smooth={true}
                             duration={1000}
-                            
+
                             className="text-lg font-medium hover:border-b-2 hover:border-purple-600 cursor-pointer"
                         >
                             Contact
@@ -199,7 +199,7 @@ const Nav = () => {
 
 
 
-                        
+
 
 
 
